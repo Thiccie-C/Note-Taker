98d7c4cf09f2b2eb53358f65612c79f73376fb81
Cameron Nelson <nelsonnoremac@gmail.com>
2022-02-12 20:27:07 -0600

    adding to heroku

--- server.js
+++ server.js
@@ -11,12 +11,13 @@ app.use(express.urlencoded({ extended: true}))
 app.use(express.json())
 
 app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/notes.html"));
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-app.get("api/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/index.html"))
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
 });
+
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"))
@@ -24,7 +25,7 @@ app.get("*", (req, res) => {
 
 app.post("/api/notes", (req, res) => {
     let newNote = req.body
-    let noteList = JSON.parse(fs.readFileSync("./db/db.json"))
+    let noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
     let notelength = (noteList.length).toString();
 
     newNote.id = notelength
